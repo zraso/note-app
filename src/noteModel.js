deeddd260ac8089c65d2a67f5e71632fe01326e3
Duncan Skinner <duncanskinner94@gmail.com>
2019-10-28 15:16:45 +0000

    writing first pure vanilla test

---
+++ src/noteModel.js
@@ -0,0 +1,15 @@
+// (function(exports) {
+//   function Circle() {
+//     this.radius = 10;
+//   };
+//
+//   exports.Circle = Circle;
+// })(this);
+// //
+
+(function(exports) {
+  function Note(text) {
+    this.text = text
+  }
+  exports.Note = Note;
+})(this)

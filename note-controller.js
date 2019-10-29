96d86fa421eacf4d090dcacb0ddcc28df14114e5
zraso <zaira.rasool@hotmail.co.uk>
2019-10-29 10:31:40 +0000

    Inserts html into page

--- note-controller.js
+++ note-controller.js
@@ -1,3 +1,14 @@
-document.getElementById('app')
 
-console.log(5)
+// console.log(elem)
+//
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   var elem = document.getElementById("app");
+//   console.log(elem);
+// })
+//
+// function changeText(newText) {
+//   document.getElementById("app").innerHTML = "Howdy!";
+// }
+// document.addEventListener("click", changeText);
+var elem = document.getElementById("app");
+elem.innerHTML = 'Howdy'

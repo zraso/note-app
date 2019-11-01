b93564bfe977e4e942165d94fb6103ace81e829a
zraso <zaira.rasool@hotmail.co.uk>
2019-11-01 12:22:31 +0000

    Almost working

--- interface.js
+++ interface.js
@@ -1,54 +1,56 @@
-list = new NoteList()
-list.addNote('Helllooooooooooooooooooooeoroeroeroe')
-list.addNote('hihi')
-
-controller = new NoteController(list)
-
-controller.insertHTML()
-
-function getNoteByID(id) {
-  return list.allNotes()[id]
-}
-
-function showNoteHTML(id) {
-  note = getNoteByID(id)
-  singleView = new SingleNoteView(note);
-  document
-    .getElementById("app")
-    .innerHTML = singleView.renderHTML()
-}
-
-function getIDfromURL() {
-  return window.location.href.split("#")[1];
-  // return window.location.href.split("#")[1];
-}
-
-window.onhashchange = function() {
-  id = getIDfromURL()
-  showNoteHTML(id)
-}
-
-function clearTextArea() {
-  document.getElementById("note").value = '';
-}
-
-document
-  .getElementById("text")
-  .addEventListener("click", function(clickEvent) {
-    clickEvent.preventDefault()
-    text = clickEvent.path[1][0].value
-    list.addNote(text)
-    controller.insertHTML()
-    clearTextArea()
-    console.log(clickEvent)
-    console.log(clickEvent.path[1][0].value)
-})
-
-document
-  .getElementById("home")
-  .addEventListener("click", function(clickEvent) {
-    clickEvent.preventDefault()
-    controller.insertHTML()
-    console.log(clickEvent)
-    console.log(clickEvent.path[1][0].value)
-})
+// list = new NoteList()
+// list.addNote('Helllooooooooooooooooooooeoroeroeroe')
+// list.addNote('hihi')
+//
+// controller = new NoteController(list)
+//
+// controller.insertHTML()
+// //
+// // function getNoteByID(id) {
+//   return list.allNotes()[id]
+// }
+//
+// function showNoteHTML(id) {
+//   note = getNoteByID(id)
+//   singleView = new SingleNoteView(note);
+//   document
+//     .getElementById("app")
+//     .innerHTML = singleView.renderHTML()
+// }
+//
+// function getIDfromURL() {
+//   return window.location.href.split("#")[1];
+//   // return window.location.href.split("#")[1];
+// }
+//
+// window.onhashchange = function() {
+//   id = getIDfromURL()
+//   showNoteHTML(id)
+// }
+//
+// function clearTextArea() {
+//   document.getElementById("note").value = '';
+// }
+//
+// document
+//   .getElementById("text")
+//   .addEventListener("click", function(clickEvent) {
+//     clickEvent.preventDefault()
+//     text = clickEvent.path[1][0].value
+//     list.addNote(text)
+//     controller.insertHTML()
+//     clearTextArea()
+//     console.log(clickEvent)
+//     console.log(clickEvent.path[1][0].value)
+// })
+//
+// document
+//   .getElementById("home")
+//   .addEventListener("click", function(clickEvent) {
+//     clickEvent.preventDefault()
+//     controller.insertHTML()
+//     console.log(clickEvent)
+//     console.log(clickEvent.path[1][0].value)
+// })
+//
+// console.log(window.location.href)
--- src/noteController.js
+++ src/noteController.js
@@ -19,18 +19,64 @@
 
 
 (function(exports){
-  function NoteController(notelist = new NoteList()) {
+  function NoteController(notelist) {
     // notelist.addNote("Favourite drink: seltzer");
-    this.noteListView = new NoteListView(notelist);
-  }
+    var notelist = notelist
+    var blah = 'hello'
+    this.notelist = notelist
+    this.noteListView = new NoteListView(this.notelist);
 
-  NoteController.prototype.insertHTML = function(doc = document) {
-    var elem = doc.getElementById("app");
-    elem.innerHTML = this.noteListView.getHTML();
-  }
+    function getIDfromURL(location) {
+      return location.hash.split("#")[1];
+    }
+
+    function showNote(id) {
+      view = new SingleNoteView(notelist.notes[id])
+      document
+        .getElementById("app")
+        .innerHTML = view.renderHTML()
+    }
+
+    function showNoteForCurrentPage() {
+      showNote(getIDfromURL(window.location));
+    }
+
+    function makeURLChangeShowNoteForCurrentPage() {
+      window.addEventListener("hashchange", showNoteForCurrentPage)
+    }
+
+    makeURLChangeShowNoteForCurrentPage();
+
+    function clearTextArea() {
+      document.getElementById("note").value = '';
+    }
+
+    document
+      .getElementById("text")
+      .addEventListener("click", function(clickEvent) {
+        clickEvent.preventDefault()
+        note = new Note(clickEvent.path[1][0].value)
+        notelist.addNote(note.text)
+        // var controller = new NoteController(list)
+        controller.insertHTML()
+        clearTextArea()
+      });
+
+    document
+      .getElementById("home")
+      .addEventListener("click", function(clickEvent) {
+        clickEvent.preventDefault()
+        controller.insertHTML()
+      })
+
+      NoteController.prototype.insertHTML = function(doc = document) {
+        var elem = doc.getElementById("app");
+        elem.innerHTML = this.noteListView.getHTML();
+      }
+
+  };
 
   exports.NoteController = NoteController;
 })(this);
 
-// controller = new NoteController();
-// controller.insertHTML();
+controller = new NoteController(new NoteList());
